25638cfd037491aea488734e4b524c82a67d2b90
shadmaanosmani <shadmanosmani@proton.me>
2026-03-15 01:50:28 +0530

    Spring test context

--- spring-di/src/test/java/guru/springframework/spring_di/SpringDiApplicationTests.java
+++ spring-di/src/test/java/guru/springframework/spring_di/SpringDiApplicationTests.java
@@ -1,10 +1,35 @@
 package guru.springframework.spring_di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.spring_di.controller.MyController;
 
 @SpringBootTest
 class SpringDiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void getControllerFromContext() {
+
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+
+	}
+	
+	@Test
+	void testAutowireOfMyController() {
+
+		System.out.println(myController.sayHello());
+
+	}
 
 	@Test
 	void contextLoads() {
